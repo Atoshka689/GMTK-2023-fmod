444ecb8d4ac0e43742ac5ba56a60ea31380b104b
Atoshka689 <focus.cosmo@gmail.com>
2025-04-01 09:22:51 +0300

    Update EnemiesManager.cs
    Count enemies and change Parameter Value "Threat"

--- Horde CORP/Assets/Scripts/EnemiesManager.cs
+++ Horde CORP/Assets/Scripts/EnemiesManager.cs
@@ -1,22 +1,49 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity; // Добавляем для работы с FMOD
 
 public class EnemiesManager : MonoBehaviour
 {
-    #region Set this class to singleton
-	static EnemiesManager _i; public static EnemiesManager i
-	{
-		get
-		{
-			if(_i==null)
-			{
-				_i = GameObject.FindObjectOfType<EnemiesManager>();
-			}
-			return _i;
-		}
-	}
-	#endregion
+    #region Singleton
+    static EnemiesManager _i; 
+    public static EnemiesManager i
+    {
+        get
+        {
+            if(_i == null) _i = GameObject.FindObjectOfType<EnemiesManager>();
+            return _i;
+        }
+    }
+    #endregion
 
-	public List<Transform> enemies = new List<Transform>();
-	public List<EnemySpawning> unlockable;
+    public List<Transform> enemies = new List<Transform>();
+    public List<EnemySpawning> unlockable;
+
+    [Header("FMOD Settings")]
+    [SerializeField] private StudioEventEmitter musicEmitter; // Эмиттер музыки FMOD
+    private int lastEnemyCount; // Запоминаем последнее количество, чтобы не проверять каждый кадр
+
+    private void Update()
+    {
+        // Проверяем только если количество изменилось
+        if (enemies.Count != lastEnemyCount)
+        {
+            UpdateFMODThreatLevel();
+            lastEnemyCount = enemies.Count;
+        }
+    }
+
+    private void UpdateFMODThreatLevel()
+    {
+        float threatLevel = (enemies.Count >= 5) ? 1f : 0f; // Threat = 1, если врагов ≥ 15
+        
+        if (musicEmitter != null)
+        {
+            musicEmitter.SetParameter("Threat", threatLevel); // Меняем параметр в FMOD
+        }
+        else
+        {
+            Debug.LogWarning("FMOD Music Emitter не назначен в EnemiesManager!");
+        }
+    }
 }
